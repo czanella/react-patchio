5355676a22e696357af323b9dd10663a4c11f0cc
Carlos Zanella <cefzanella@gmail.com>
2020-11-04 11:28:25 -0300

    Using conditional chaining to remove TS error message

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,4 @@ ReactDOM.render(
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
+import.meta?.hot?.accept();

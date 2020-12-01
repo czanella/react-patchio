d1bcf6a756e013f7c94dad2e1c12f7dd5f42af03
Carlos Zanella <cefzanella@gmail.com>
2020-12-01 11:46:28 -0300

    Patch is now draggable

--- src/patch/patch.tsx
+++ src/patch/patch.tsx
@@ -6,25 +6,37 @@ import styles from './styles.module.scss';
 interface PatchProps extends IPosition {
   className?: string,
   name: string,
+  // eslint-disable-next-line no-unused-vars
+  onDrag(patchId: string, x: number, y: number): void,
   order: number,
+  patchId: string,
+}
+
+interface DragPosition extends IPosition {
+  initialX: number,
+  initialY: number,
 }
 
 const Patch = ({
-  className, name, order, x, y,
+  className, name, onDrag = () => {}, order, patchId, x, y,
 } : PatchProps) => {
   const style = { transform: `translate(${x}px, ${y}px` };
-  const [dragPosition, setDragPosition] = useState<IPosition | null>(null);
+  const [dragPosition, setDragPosition] = useState<DragPosition | null>(null);
 
   const onDragStart = useCallback((e : React.MouseEvent) => {
-    setDragPosition({ x: e.screenX, y: e.screenY });
-  }, [setDragPosition]);
+    setDragPosition({
+      x: e.screenX,
+      y: e.screenY,
+      initialX: x,
+      initialY: y,
+    });
+  }, [setDragPosition, x, y]);
 
   useEffect(() => {
     if (dragPosition) {
-      const onDragMove = (e : MouseEvent) => console.log({
-        x: e.screenX - dragPosition.x,
-        y: e.screenY - dragPosition.y,
-      });
+      const onDragMove = (e : MouseEvent) => onDrag(patchId,
+        e.screenX - dragPosition.x + dragPosition.initialX,
+        e.screenY - dragPosition.y + dragPosition.initialY);
       const onDragEnd = () => window.removeEventListener('mousemove', onDragMove);
 
       window.addEventListener('mousemove', onDragMove);
--- src/patchio/patchio.tsx
+++ src/patchio/patchio.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import Patch from '../patch';
 
 import styles from './styles.module.scss';
@@ -7,8 +7,16 @@ interface PatchioProps {
   className?: string,
 }
 
+interface PatchData {
+  [id: string]: {
+    name: string,
+    x: number,
+    y: number,
+  },
+}
+
 const Patchio = ({ className }: PatchioProps) => {
-  const [patchData] = useState({
+  const [patchData, setPatchData] = useState<PatchData>({
     1: {
       name: 'Multiply',
       x: 30,
@@ -16,12 +24,29 @@ const Patchio = ({ className }: PatchioProps) => {
     },
   });
 
+  const onPatchDrag = useCallback((patchId: string, x: number, y: number) => {
+    if (!patchData[patchId]) {
+      return;
+    }
+
+    setPatchData({
+      ...patchData,
+      [patchId]: {
+        ...patchData[patchId],
+        x,
+        y,
+      },
+    });
+  }, []);
+
   const patches = useMemo(() => Object
     .entries(patchData).map(([key, { name, x, y }], i) => (
       <Patch
         key={key}
         name={name}
+        onDrag={onPatchDrag}
         order={i}
+        patchId={key}
         x={x}
         y={y}
       />
